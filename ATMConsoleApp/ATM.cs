031ff5646cbb2bc8cb7631ff0bddd945370ad760
Linnova <92028792+Linnova98@users.noreply.github.com>
2022-06-30 16:47:31 +0200

    16:47

---
+++ ATMConsoleApp/ATM.cs
@@ -0,0 +1,57 @@
+﻿namespace ATMConsoleApp;
+
+public class ATM
+{
+    public List<CardHolder> CardHolders { get; set; }
+
+    public ATM(List<CardHolder> cardHolders)
+    {
+       CardHolders = cardHolders;
+    }
+
+
+
+    void printOptions()
+    {
+        Console.WriteLine("Please choose from one of the following options...");
+        Console.WriteLine("1. Deposit");
+        Console.WriteLine("2. Withdraw");
+        Console.WriteLine("3. Show Balance");
+        Console.WriteLine("4. Exit");
+    }
+    void deposit(CardHolder currentUser)
+    {
+        Console.WriteLine("How much $$ would you like to deposit?");
+        double deposit = Double.Parse(Console.ReadLine());
+        currentUser.setBalance(deposit);
+        Console.WriteLine("Thank you for your $$. Your new balance is: " + currentUser.GetBalance());
+
+
+        //gammel.
+        Console.WriteLine("How much $$ would you like to deposit?");
+        double deposit = Double.Parse(Console.ReadLine());
+        currentUser.setBalance(currentUser.GetBalance() + deposit);
+        Console.WriteLine("Thank you for your $$. Your new balance is: " + currentUser.GetBalance());
+    }
+
+    void withdraw(CardHolder currentUser)
+    {
+        Console.WriteLine("How much $$ would you like to withdraw?");
+        double withdrawal = Double.Parse(Console.ReadLine());
+        //Check if the user has enough money
+        if (currentUser.GetBalance() < withdrawal)
+        {
+            Console.WriteLine("Insufficent balance :(");
+        }
+        else
+        {
+            currentUser.setBalance(currentUser.GetBalance() - withdrawal);
+            Console.WriteLine("You're good to go! Thank you :)");
+        }
+    }
+
+    void balance(CardHolder currentUser)
+    {
+        Console.WriteLine("Current balance: " + currentUser.GetBalance());
+    }
+}
---
+++ ATMConsoleApp/CustomerDatabase.cs
@@ -0,0 +1,16 @@
+﻿namespace ATMConsoleApp;
+
+public class CustomerDatabase
+{
+    public static void MakeHolders()
+    {
+        List<CardHolder> cardHolders = new List<CardHolder>();
+
+        cardHolders.Add(new CardHolder("4532772818527395", 1234, "John", "Griffith", 150.31));
+        cardHolders.Add(new CardHolder("4532761841325802", 4321, "ASHLEY", "JONES", 321.13));
+        cardHolders.Add(new CardHolder("5128381368581872", 9999, "Frida", "Dickerson", 105.59));
+        cardHolders.Add(new CardHolder("6011188364697109", 2468, "MUNEEB", "HARDING", 851.84));
+        cardHolders.Add(new CardHolder("3490692153147110", 4826, "DAWN", "SMITH", 54.27));
+    }
+    
+}
