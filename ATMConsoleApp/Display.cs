9b6009bf36e845e20c6ed0c68719e45b0826b8a3
Linnova <92028792+Linnova98@users.noreply.github.com>
2022-06-30 20:24:08 +0200

    20:24

--- ATMConsoleApp/Display.cs
+++ ATMConsoleApp/Display.cs
@@ -2,5 +2,17 @@
 
 public class Display
 {
-    
+    void printOptions()
+    {
+        Console.WriteLine("Please choose from one of the following options...");
+        Console.WriteLine("1. Deposit");
+        Console.WriteLine("2. Withdraw");
+        Console.WriteLine("3. Show Balance");
+        Console.WriteLine("4. Exit");
+    }
+
+    //switch
+    //valgt deposit.
+    //input = convert.double.readline
+    //ValgteCardHolder.Deposit(input);
 }
--- ATMConsoleApp/Program.cs
+++ ATMConsoleApp/Program.cs
@@ -1,11 +1,123 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Channels;
 using ATMConsoleApp;
 
-internal class Program
+public class Program 
 {
     public static void Main(String[] args)
     {
+        CustomerDatabase.MakeHolders();
+        StartATM();
 
+
+        switch ()
+        {
+            case: "1"
+        }
+
+
+    }
+
+
+
+
+
+    public static void StartATM()
+    {
+        //Prompt user
+        Console.WriteLine("Welcome to SimpleATM");
+        Console.WriteLine("Please insert your debit card: ");
+        String debitCardNum = "";
+        CardHolder currentUser;
+
+        while (true)
+        {
+            try
+            {
+                debitCardNum = Console.ReadLine();
+                // Check against our db
+                currentUser = cardHolders.FirstOrDefault(a => a.CardNum == debitCardNum);
+                if (currentUser != null)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Card not recognized. Please try again");
+                }
+
+            }
+            catch
+            {
+                Console.WriteLine("Card not recognized. Please try again");
+            }
+
+        }
+
+
+
+
+
+
+
+
+
+        Console.WriteLine("Please enter your pin: ");
+        int userPin = 0;
+        while (true)
+        {
+            try
+            {
+                userPin = int.Parse(Console.ReadLine());
+                // Check against our db
+                
+                if (currentUser.GetPin() == userPin)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect pin. Please try again");
+                }
+
+            }
+            catch
+            {
+                Console.WriteLine("Incorrect pin. Please try again");
+            }
+        }
+
+        Console.WriteLine("Welcome " + currentUser.GetFirstName() + " :)");
+        int option = 0;
+        do
+        {
+            printOptions();
+            try
+            {
+                option = int.Parse(Console.ReadLine());
+            }
+            catch { }
+            if(option == 1)
+            {
+                deposit(currentUser);
+
+            }
+            else if(option == 2){ withdraw(currentUser); }
+            else if (option == 3) { balance(currentUser); }
+            else if (option == 4)
+            {
+                break;
+            }
+            else
+            {
+                option = 0;
+
+            }
+
+
+        } while (option != 4);
+
+        Console.WriteLine("Thank you! Have a nice day :)");
     }
 }
